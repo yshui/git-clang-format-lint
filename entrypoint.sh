cc2b5025de1e13726729ab574565bcf227fcf04b
Yuxuan Shui <yshuiv7@gmail.com>
2020-04-09 00:45:09 +0100

    Add base

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd "$GITHUB_WORKSPACE"
 
-diff=`git-clang-format --diff`
+diff=`git-clang-format --diff --commit $INPUT_BASE`
 
 echo $diff | colordiff
 [ "$diff" = "no modified files to format" ] || exit 1
