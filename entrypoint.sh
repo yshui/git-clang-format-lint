3aa62e0b28165b0e4bcf0aa440b4f1f7c991fd05
Yuxuan Shui <yshuiv7@gmail.com>
2020-04-09 01:06:46 +0100

    fix echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -l
 
-set -x
-
 cd "$GITHUB_WORKSPACE"
 
 diff=`git-clang-format --diff --commit $INPUT_BASE`
 
-echo $diff | colordiff --color=always
+echo "$diff" | colordiff --color=always
 [ "$diff" = "no modified files to format" ] || exit 1
