eb6e591e01d97cffdd9d6cf8cae2ee3044b3bf32
Yuxuan Shui <yshuiv7@gmail.com>
2020-04-09 14:42:04 +0100

    Print a message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 cd "$GITHUB_WORKSPACE"
 
 diff=`git-clang-format --diff --commit $INPUT_BASE`
+[ "$diff" = "no modified files to format" ] && exit 0
+
+printf "\033[1mYou have introduced coding style breakages, suggested changes:\n\n"
 
 echo "$diff" | colordiff
-[ "$diff" = "no modified files to format" ] || exit 1
+exit 1
