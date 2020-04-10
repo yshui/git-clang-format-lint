a90850cbdc4f6eb176239b162e0bdd6a430dd857
Yuxuan Shui <yshuiv7@gmail.com>
2020-04-10 04:07:35 +0100

    Add more success conditions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ cd "$GITHUB_WORKSPACE"
 
 diff=`git-clang-format --diff --commit $INPUT_BASE`
 [ "$diff" = "no modified files to format" ] && exit 0
+[ "$diff" = "clang-format did not modify any files" ] && exit 0
 
 printf "\033[1mYou have introduced coding style breakages, suggested changes:\n\n"
 
